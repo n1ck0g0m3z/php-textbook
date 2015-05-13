6e1112209c2639d87512b0cc0e421c7ba1be677f
Christian Nicolas Gomez Santana <christiangomez1992@gmail.com>
2015-05-13 04:37:35 +0000

    0513 practice

---
+++ sample03.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+        date_default_timezone_set('Asia/Tokyo');
+        print('現在' . date('G時 i分 s秒') . 'です');
+    ?>
+</body>
+</html>
---
+++ sample04.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        print('お名前：' . htmlspecialchars($_REQUEST['my_name'], ENT_QUOTES));
+    ?>
+</body>
+</html>
